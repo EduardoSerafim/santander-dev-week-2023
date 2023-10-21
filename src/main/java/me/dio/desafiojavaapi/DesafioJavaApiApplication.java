8667077216d10e3173d461bbe91381f91446a598
Eduardo Serafim <eduserafim7@gmail.com>
2023-10-21 18:30:50 -0300

    Initial commit

---
+++ src/main/java/me/dio/desafiojavaapi/DesafioJavaApiApplication.java
@@ -0,0 +1,13 @@
+package me.dio.desafiojavaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioJavaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioJavaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/dio/desafiojavaapi/DesafioJavaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.desafiojavaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioJavaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
